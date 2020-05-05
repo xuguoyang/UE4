eb1b544d100a270e3e9b939d00a05e000a602777
xugy <wxkyxgy@foxmail.com>
2020-05-05 14:48:03 +0800

    整理代码目录

---
+++ MyActionRPG/Source/MyActionRPG/Private/MyActionRPGGameModeBase.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActionRPGGameModeBase.h"
+
+AMyActionRPGGameModeBase::AMyActionRPGGameModeBase()
+{
+    // 设置GameState、PlayerController所使用的UClass对象
+    //GameStateClass =
+}
---
+++ MyActionRPG/Source/MyActionRPG/Public/RPGGameInstanceBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "RPGGameInstanceBase.generated.h"
+
+/**
+ * 生成基于此类的蓝图，在蓝图中添加相关的逻辑
+ * 主要负责游戏数据的保存，和游戏对应状态的事件处理
+ */
+UCLASS()
+class MYACTIONRPG_API URPGGameInstanceBase : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+
+};
